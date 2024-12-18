fd0e5574f99098208e02c89ea987166346a62259
Johncosgrove7421 <johncosgrove7421@gmail.com>
2024-12-18 06:52:14 -0500

    update subnets with for_each meta_argument

--- three_tier_arch/subnets.tf
+++ three_tier_arch/subnets.tf
@@ -1,8 +1,10 @@
 ### Subnets
 
 resource "aws_subnet" "subnet" {
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.vpc.id
+  for_each          = var.subnets
+  cidr_block        = each.vaule.cidr_block
+  availability_zone = each.vaule.az
 
   tags = {
     Name = "Executive Presentation"
--- three_tier_arch/var.tf
+++ three_tier_arch/var.tf
@@ -1,9 +1,15 @@
-variable "cidrs" {
-  type = list(string)
-  default = ["10.0.1.0/24", 
-             "10.0.2.0/24",
-             "10.0.3.0/24",
-             "10.0.4.0/24",
-             "10.0.5.0/24",
-             "10.0.6.0/24"]
+variable "subnets" {
+  description = "List of subnets to create"
+  type        = map(object({
+    cidr_block = string
+    az         = string
+  }))
+  default = {
+    subnet1 = { cidr_block = "10.0.1.0/24", az = "us-east-1a" }
+    subnet2 = { cidr_block = "10.0.2.0/24", az = "us-east-1a" }
+    subnet3 = { cidr_block = "10.0.3.0/24", az = "us-east-1b" }
+    subnet4 = { cidr_block = "10.0.4.0/24", az = "us-east-1b" }
+    subnet5 = { cidr_block = "10.0.5.0/24", az = "us-east-1c" }
+    subnet6 = { cidr_block = "10.0.6.0/24", az = "us-east-1c" }
+  }
 }
