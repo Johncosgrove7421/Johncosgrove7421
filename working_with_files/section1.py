9832559477fbcb01751aace21ff27629dd24186b
Johncosgrove7421 <johncosgrove7421@gmail.com>
2024-12-29 19:40:05 -0500

    Assignment #5

---
+++ working_with_files/section1.py
@@ -0,0 +1,15 @@
+#//*********************************************************
+
+#//****Assignment 5 Section 1
+
+#//*********************************************************
+
+import Pandas
+
+produceDict = {'Fruit': ['bananas', 'grapes', 'apples', 'pears', 'lettuce', 'onions', 'potatoes', 'peaches'], 'Price': [0.59, 2.99, 1.49, 1.39, 0.99, 0.79, 0.58, 1.59]} 
+
+
+def fileLineCount():
+  print('Hello World') > ProducePrice.txt
+
+fileLineCount()
