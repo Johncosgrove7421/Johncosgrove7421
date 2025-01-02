e53f50f7746093371a4631e2d0ab9c2053f50e27
Johncosgrove7421 <johncosgrove7421@gmail.com>
2025-01-01 20:00:36 -0500

    update working with files

--- working_with_files/section1.py
+++ working_with_files/section1.py
@@ -1,15 +1,53 @@
+import pandas as pd
+import numpy as np
+import os
 #//*********************************************************
 
 #//****Assignment 5 Section 1
 
 #//*********************************************************
 
-import Pandas
+print('Section 1')
+# Add the following data to the produce dictionary by adding key names as Fruit and Price.
 
-produceDict = {'Fruit': ['bananas', 'grapes', 'apples', 'pears', 'lettuce', 'onions', 'potatoes', 'peaches'], 'Price': [0.59, 2.99, 1.49, 1.39, 0.99, 0.79, 0.58, 1.59]} 
+produceDict = pd.DataFrame({'Fruit': ['bananas', 'grapes', 'apples', 'pears', 'lettuce', 'onions', 'potatoes', 'peaches'], 'Price': [0.59, 2.99, 1.49, 1.39, 0.99, 0.79, 0.58, 1.59]})
+produceDict.index = np.arange(1, len(produceDict) + 1)
+produceDict.to_string('ProducePrice.txt')
+
+def fileLineCount(file):
+  df = pd.read_csv(f'{file}.txt')
+  num_rows = df.shape[0]
+  print(f'There are {num_rows} in the file, where {num_rows} is the number returned by fileLineCount()')
+
+fileLineCount(file='ProducePrice')
+
+#//*********************************************************
+
+#//****Assignment 5 Section 2
+
+#//*********************************************************
+
+print('Section 2')
+# add four more items to the produceDict and append them to the ProducePrice.txt file
 
 
-def fileLineCount():
-  print('Hello World') > ProducePrice.txt
+['peppers', 'celery', 'cabbage', 'tomatoes']
+[0.99, 1.29, 0.79, 1.19]
 
-fileLineCount()
+
+# UPDATE
+# print(f'There are {len(produceDict["Fruit"])} in the file, where {len(produceDict["Fruit"])} is the number returned by fileLineCount()')
+
+fileLineCount(file='ProducePrices')
+
+#//*********************************************************
+
+#//****Assignment 5 Section 3
+
+#//*********************************************************
+
+print('Section 3')
+produceDict.index = np.arange(1, len(produceDict) + 1)
+print(produceDict)
+# UPDATE
+#print(f'There are {len(produceDict["Fruit"])} in the file, where {len(produceDict["Fruit"])} is the number returned by fileLineCount()')
