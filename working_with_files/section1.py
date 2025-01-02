cac91aeabffa7594395d015bbd3ff1b3e0009b66
Johncosgrove7421 <johncosgrove7421@gmail.com>
2025-01-02 10:34:57 -0500

    program is producing expected output

--- working_with_files/section1.py
+++ working_with_files/section1.py
@@ -1,25 +1,22 @@
 import pandas as pd
 import numpy as np
-import os
 #//*********************************************************
 
 #//****Assignment 5 Section 1
 
 #//*********************************************************
 
-print('Section 1')
-# Add the following data to the produce dictionary by adding key names as Fruit and Price.
+print('Section 1') # Add the following data to the produce dictionary by adding key names as Fruit and Price.
 
 produceDict = pd.DataFrame({'Fruit': ['bananas', 'grapes', 'apples', 'pears', 'lettuce', 'onions', 'potatoes', 'peaches'], 'Price': [0.59, 2.99, 1.49, 1.39, 0.99, 0.79, 0.58, 1.59]})
-produceDict.index = np.arange(1, len(produceDict) + 1)
-produceDict.to_string('ProducePrice.txt')
+produceDict.to_string('ProducePrice.txt', index=False)
 
 def fileLineCount(file):
   df = pd.read_csv(f'{file}.txt')
   num_rows = df.shape[0]
   print(f'There are {num_rows} in the file, where {num_rows} is the number returned by fileLineCount()')
 
-fileLineCount(file='ProducePrice')
+fileLineCount('ProducePrice')
 
 #//*********************************************************
 
@@ -27,18 +24,20 @@ fileLineCount(file='ProducePrice')
 
 #//*********************************************************
 
-print('Section 2')
-# add four more items to the produceDict and append them to the ProducePrice.txt file
+print('\nSection 2') # add four more items to the produceDict and append them to the ProducePrice.txt file
 
+new_data = {
+  'Fruit': ['celery', 'cabbage', 'tomatoes', 'peppers'],
+  'Price': [1.29, 0.79, 1.19, 0.99],
+}
 
-['peppers', 'celery', 'cabbage', 'tomatoes']
-[0.99, 1.29, 0.79, 1.19]
+new_row = pd.DataFrame(new_data)
+df = pd.concat([produceDict, new_row])
+df = df.reset_index(drop=True)
+df.to_string('ProducePrice.txt', index=False)
 
+fileLineCount('ProducePrice')
 
-# UPDATE
-# print(f'There are {len(produceDict["Fruit"])} in the file, where {len(produceDict["Fruit"])} is the number returned by fileLineCount()')
-
-fileLineCount(file='ProducePrices')
 
 #//*********************************************************
 
@@ -46,8 +45,10 @@ fileLineCount(file='ProducePrices')
 
 #//*********************************************************
 
-print('Section 3')
-produceDict.index = np.arange(1, len(produceDict) + 1)
-print(produceDict)
-# UPDATE
-#print(f'There are {len(produceDict["Fruit"])} in the file, where {len(produceDict["Fruit"])} is the number returned by fileLineCount()')
+print('\nSection 3')
+producePrices = pd.read_csv(f'ProducePrice.txt')
+producePrices = producePrices.reset_index(drop=True)
+producePrices.index = np.arange(1, len(producePrices) + 1)
+print(producePrices)
+
+fileLineCount('ProducePrice')
