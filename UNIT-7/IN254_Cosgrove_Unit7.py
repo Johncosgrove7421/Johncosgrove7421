afc99d09112069d0e1c173efdf87e7b029261964
Johncosgrove7421 <johncosgrove7421@gmail.com>
2025-01-28 19:17:28 -0500

    working

--- UNIT-7/IN254_Cosgrove_Unit7.py
+++ UNIT-7/IN254_Cosgrove_Unit7.py
@@ -1,4 +1,4 @@
-import sys
+import os
 import logging
 
 # docs
@@ -10,78 +10,85 @@ print('\nAll console error messages are printed from error log file.')
 
 # Logging to a file.
 
-log_file = "stderr_log.txt"
-logging.basicConfig(
-    filename=log_file,
-    level=logging.ERROR,
-    format="ERROR:root%(message)s",
+if (os.path.exists("logFile.txt")):
+  os.remove("logFile.txt")
 
-)
+# Logging for standard error
 
-# Redirecting sys.stderr to the file
-sys.stderr = open(log_file, 'a')
+logger = logging.getLogger('')
+logging.basicConfig(filename="logFile.txt")
+stderrLogger=logging.StreamHandler()
+stderrLogger.setFormatter(logging.Formatter(logging.BASIC_FORMAT))
+logging.getLogger().addHandler(stderrLogger)
 
+# Open and read log file
+def displayLogFile():
+  try:
+       f = open("logFile.txt", "r")
+       print(f.read())
+  except:
+       print('Failed to read log file')
 
-# Read the log file and display to the console
-def displayLogfile(logs):
-  with read(logs, 'r') as file:
-    contents = file.read(logs)
-  print('Show the contents of the log file.')
-  print(contents)
- 
+# Function to divide by zero
+def divideByZero():
+   x = 33
+   y = 0
+   if y == 0:
+     raise Exception("Attempted to divide by zero.")
+   return x/y
 
-# Divide a number by 0
-def divideByZero(number):
-  33 / number
-  raise ZeroDivisionError("Attempted to divide by zero.") 
-   
+# Open a non existant file
+def fileDoesNotExist():
+   try:
+     f = open("NoFileNamedThis.txt", "r")
+   except:
+     raise Exception("Could not find file NoFileNamedThis.txt")
 
+# Out of Bounds
+def arrayOutOfBounds():
+   names = ["ed", "john", "kenz", "Mel"]
+   someName = []
 
-# fileDoesNotExist function
+   # Test and print exception
+   try:
+        for i in range(0, (len(names) +1)):
+            if(i >= len(names)):
+                raise Exception("Index was outside the bounds of the array.")
+   except Exception as ae:
+       raise Exception("Index was outside the bounds of the array")
 
-def fileDoesNotExist(filename):
-  with read(filename, 'r') as file:
-    file_contents = file.read(filename)
-  raise FileNotFoundError("Could not find file NoFileNamedThis.txt.")
-
-
-# Array OutOfbound
-def arrayOutOfBounds(name):
-  bounds = [ "out", "of", "boundz"]
-  for item in name[6]:
-    print(item)
-  raise IndexError("Index was outside the bounds of the array.")
-  
-
-# Nullarray
+# null array
 def arrayIsNull():
-  my_array = []
-  my_array = list()
-  print(my_array)
-  raise ValueError("Object reference not set to an instance of an object.")
-
+    myArr = []
+    try:
+        print(myArr[1])
+    except Exception as ae:
+         raise Exception("Object reference not set to an instance of an object")
 
 # Calling all functions
 
 try:
     divideByZero()
-except ZeroDivisionError as e:
-    logging.error(e)
+except Exception as ae:
+    logger.error(ae)
 
 try:
     fileDoesNotExist()
-except FileNotFoundError as e:
-    logging.error(e)
+except Exception as ae:
+    logger.error(ae)
 
 try:
     arrayOutOfBounds()
-except IndexError as e:
-    logging.error(e)
+except Exception as ae:
+    logger.error(ae)
 
 try:
     arrayIsNull()
-except ValueError as e:
-    logging.error(e)
+except Exception as ae:
+    logger.error(ae)
 
 # Display the log file contents
-displayLogfile(log_file)
+print('\n\nShow the contents of the log file')
+displayLogFile()
+
+
